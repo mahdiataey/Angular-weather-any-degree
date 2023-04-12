160aecbfe0cc2930f37e5e14b61aebbc9466847d
mahdyataey <ataey.2012@gmail.com>
2023-04-12 21:10:05 +0430

    send the build project

--- projects/angular-weather-any-degree/src/lib/config.ts
+++
@@ -1,1 +0,0 @@
-export const API = 'https://api.openweathermap.org/data/2.5/weather?';
--- projects/angular-weather-any-degree/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-weather-any-degree
- */
-
-export * from './lib/angular-weather-any-degree.service';
-export * from './lib/angular-weather-any-degree.component';
-export * from './lib/angular-weather-any-degree.module';
